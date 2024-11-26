9c7a27530e02a3369cd984a84ae0b08f798df9c4
JloaizaL <jloaizal@unal.edu.co>
2024-11-25 23:56:08 -0500

    prueba 1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,26 +1,17 @@
 package classroom;
 
 public class Persona {
-
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
-    public Persona(long cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
-    
-    public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
+    public Persona() {
+        this.cedula = totalPersonas++;  
+        this.nombre = "";
     }
 
     public Persona(long cedula) {
@@ -30,10 +21,16 @@ public class Persona {
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.nombre = nombre;
+        this.cedula = totalPersonas++;  
+    }
+
+    public Persona(long cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
         totalPersonas++;
     }
-    
+
     public long getCedula() {
         return cedula;
     }
